eecb6d053c28a1ae00e6bcb0a1aad2db99c43557
Genki Aikawa <ga07600@uga.edu>
2022-04-25 14:45:28 -0400

    Boilerplate interview

--- hacksInterview/eventList/admin.py
+++ hacksInterview/eventList/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from .models import Event
-
 # TODO: Register models for admin site
 # Import model
-admin.site.register(Event) 
+
--- hacksInterview/eventList/views.py
+++ hacksInterview/eventList/views.py
@@ -5,10 +5,5 @@ from .models import Event
 # Create your views here.
 def homePage(request):
     # Query the Event
-    events = Event.objects.all()
 
-    context = {
-        'events': events
-    }
-
-    return render(request, 'eventList/home.html', context)
+    return render(request, 'eventList/home.html')
